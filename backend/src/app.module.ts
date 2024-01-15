c44255a2022e08afb0dda95992fe82debcd410dd
Vilaphong Douangmala <vilaphong.d@codium.co>
2024-01-15 23:09:54 +0700

    BE: init user entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfigs from './configs/db.configs';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,46 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
+
+  @Column({
+    length: 50,
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+    name: 'username',
+  })
+  @ApiProperty()
+  username: string;
+
+  @Column({ length: 50, type: 'varchar', nullable: false, name: 'password' })
+  @ApiProperty()
+  password: string;
+
+  @Column({ length: 50, type: 'varchar', nullable: false, name: 'first_name' })
+  @ApiProperty()
+  firstName: string;
+
+  @Column({ length: 50, type: 'varchar', nullable: true, name: 'last_name' })
+  @ApiProperty()
+  lastName: string;
+
+  @Column({ length: 50, type: 'varchar', nullable: false })
+  @ApiProperty()
+  email: string;
+
+  @Column({ type: 'timestamptz', name: 'date_of_birth', nullable: false })
+  @ApiProperty()
+  dateOfBirth: Date;
+
+  @Column({ length: 50, type: 'varchar', nullable: false })
+  @ApiProperty()
+  country: string;
+
+  @Column({ type: 'boolean', default: false })
+  isSuperUser: boolean;
+}
