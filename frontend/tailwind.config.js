7e957fa4e2d3c7596d33faf02b49a7c2aed2e751
Vilaphong Douangmala <vilaphong.d@codium.co>
2024-01-25 23:41:19 +0700

    FE: added daisyUI and revise login form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        defaultTheme: {
+          "primary": "#111827",
+          "secondary": "#1f2937",
+          "accent": "#fafafa",
+          "neutral": "#374151", 
+          "base-100": "#1f2937",
+        },
+      },
+    ],
+  },
 };
